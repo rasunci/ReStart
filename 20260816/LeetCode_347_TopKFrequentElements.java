16b59ac8f7f8d4be287a75c816455928504b14cc
Ryan Asuncion <ryan@MBP16.local>
2026-08-16 15:40:05 -0700

    LeetCode_347_TopKFrequentElements.java

--- 20260816/LeetCode_347_TopKFrequentElements.java
+++ 20260816/LeetCode_347_TopKFrequentElements.java
@@ -3,25 +3,33 @@ import java.util.*;
 class Solution {
 
     public int[] topKFrequent(int[] nums, int k) {
+
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-			map.put(num, map.getOrDefault(num, 0) + 1);
+			if (!map.containsKey(num)) {
+				map.put(num, 0);
+			}
+			map.put(num, map.get(num) + 1);
         }
+
         Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
 			(a, b) -> {
-	            return Integer.compare(a.getValue(), b.getValue());
+				return Integer.compare(a.getValue(), b.getValue());
 			}
         );
+
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             pq.add(entry);
-            while (pq.size() > k) {
+            if (pq.size() > k) {
                 pq.poll();
             }
         }
+
         int[] ans = new int[k];
         for (int i = 0; i < k; i++) {
             ans[i] = pq.poll().getKey();
         }
+
         return ans;
     }
 }
