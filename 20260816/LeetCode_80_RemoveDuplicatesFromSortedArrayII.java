cad102c9e79050e17d547d92a80a9e0f91ed3d08
Ryan Asuncion <ryan@MBP16.local>
2026-08-16 17:59:38 -0700

    LeetCode_80_RemoveDuplicatesFromSortedArrayII.java

---
+++ 20260816/LeetCode_80_RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,15 @@
+class Solution {
+
+	public int removeDuplicates(int[] nums) {
+
+		int index = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (index > 1 && nums[index - 2] == nums[i]) {
+				continue;
+			}
+			nums[index++] = nums[i];
+		}
+		return index;
+	}
+}
