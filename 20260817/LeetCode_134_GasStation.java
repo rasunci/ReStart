9d0bec78f2963bd6344c8da62271568ca29bea7e
Ryan Asuncion <ryan@MBP16.local>
2026-08-17 13:16:37 -0700

    LeetCode_134_GasStation.java

---
+++ 20260817/LeetCode_134_GasStation.java
@@ -0,0 +1,27 @@
+class Solution {
+
+	public int canCompleteCircuit(int[] gas, int[] cost) {
+
+		int total = 0;
+		int current = 0;
+		int start = 0;
+
+		for (int i = 0; i < gas.length; i++) {
+
+			// Can circuit complete
+			total   += gas[i] - cost[i];
+			current += gas[i] - cost[i];
+
+			if (current < 0) {
+				current = 0;
+				start = i + 1;
+			}
+		}
+
+		if (total < 0) {
+			return -1;
+		}
+
+		return start;
+	}
+}
