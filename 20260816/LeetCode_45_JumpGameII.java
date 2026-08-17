522bc9caf558e31e4fde45ecc9f1c44e4bac58d3
Ryan Asuncion <ryan@MBP16.local>
2026-08-16 20:44:20 -0700

    LeetCode_45_JumpGameII.java

---
+++ 20260816/LeetCode_45_JumpGameII.java
@@ -0,0 +1,24 @@
+class Solution {
+
+	public int jump(int[] nums) {
+
+		int jumps = 0;
+		int range = 0;
+		int farthest = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (i == nums.length - 1) {
+				return jumps;
+			}
+
+			farthest = Math.max(farthest, nums[i] + i);
+
+			if (i == range) {
+				jumps++;
+				range = farthest;
+			}
+		}
+
+		return jumps;
+	}
+}
