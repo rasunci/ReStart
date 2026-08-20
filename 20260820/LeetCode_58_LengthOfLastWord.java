f71ec60bfb88a29aa64058971c2ed6c8e198dc6e
Ryan Asuncion <ryan@MBP16.local>
2026-08-20 13:34:07 -0700

    LeetCode_58_LengthOfLastWord.java

---
+++ 20260820/LeetCode_58_LengthOfLastWord.java
@@ -0,0 +1,17 @@
+class Solution {
+
+	public int lengthOfLastWord(String s) {
+
+		int r = s.length() - 1;
+		while (r >= 0 && s.charAt(r) == ' ') {
+			r--;
+		}
+
+		int l = r;
+		while (l >= 0 && s.charAt(l) != ' ') {
+			l--;
+		}
+
+		return r - l;
+	}
+}
